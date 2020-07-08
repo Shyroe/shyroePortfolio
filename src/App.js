3cae9fba21b4cea97b10b97e338490f8a83f532e
Shyroe <leo.henrique747@gmail.com>
2020-07-08 13:07:13 -0300

    Feat: add location and footer pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Portfolio from './components/Portfolio/index';
 import About from './components/About';
 import Contact from './components/Contact';
 import Location from './components/Location';
+import Footer from './components/Footer';
 
 const App = () => (
   <>
@@ -18,6 +19,7 @@ const App = () => (
     <About />
     <Contact />
     <Location />
+    <Footer />
   </>
 );
 
---
+++ src/components/Footer/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import V from '../../styles/variables';
+
+export const FooterSection = styled.section`
+  padding: 2% 0;
+  background-color: ${V.colorIcon};
+  text-align: center;
+
+  & > .description {
+    font-size: 1.6rem;
+  }
+`;
